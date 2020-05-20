8386331fbe0fe23bb756ae3052e50a62c863a427
pauline-mfr <p.monfrance@codeur.online>
2020-05-20 10:46:49 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Newsletter</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+<div class="header">
+  <img src="beach_img" alt="beach">
+  <img class="logo" src="logo" alt="logo odin">
+  <nav>
+    <a href="#Home">Home</a>
+    <a href="#Apps">Apps</a>
+    <a href="#Blog">Blog</a>
+  </nav>
+  <h1>Modern and Clean Newsletter Template for your Agency</h1>
+<!--insérer bouton flèche-->
+</div>
+<div class="apps">
+  <div class="app1">
+    <!--insérer logo lampe-->
+    <h2>Great Ideas</h2>
+    <p>Creative Minds on Action</p>
+  </div>
+  <div class="app2">
+    <!--insérer logo TV-->
+    <h2>Advertise With Us</h2>
+    <p>We are Big and Loud</p>
+  </div>
+  <div class="app3">
+    <!--insérer logo prise-->
+    <h2>Work With Us</h2>
+    <p>Work is Our Second Name</p>
+  </div>
+</div>
+<div class="Office">
+  <div class="we_offer">
+    <h2>We offer modern and clean newsletter design</h2>
+    <ul>
+      <li>Responsive Design</li>
+      <li>Modern and Clean</li>
+      <li>Help File Included</li>
+    </ul>
+    <!--insérer bouton Learn More-->
+  </div>
+  <img src="desk_img" alt="desk">
+  </div>
+  <div class="video">
+    <img src="mountain_img" alt="mountain">
+    <h1>WATCH OUR VIDEO</h1>
+    <!--insérer bouton Play Now-->
+  </div>
+  <div class="blog">
+    <h1>Some news from our blog</h1>
+    <p>Cras ultricies ligula seo</p>
+    <div class="post1">
+      <img src="wave_img" alt="wave">
+      <h2>Some Random Text</h2>
+        <p>Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
+<!--insérer bouton Read More-->
+    </div>
+    <div class="post2">
+      <img src="seagull_img" alt="seagull">
+      <h2>Some Random Text</h2>
+        <p>Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
+<!--insérer bouton Read More-->
+    </div>
+    <!-- insérer bouton +  -->
+  </div>
+  <div class="Advertise">
+    <h1>Start your web agency for just $19</h1>
+    <p>Cras ultricies ligula sed magna dictum porta.</p>
+    <!-- insérer boutons Start Project ET Read More-->
+  </div>
+  <div class="contact">
+    <div class="brand"
+      <img class="logo" src="logo" alt="logo odin">
+      <p>Nulla quis lorem ut libero malesuada feugiat. Maurisblandit aliquet elit, eget tincidunt nibh pulvinar est name albis tinchodion esto nih deoslous</p>
+    </div>
+    <div class="medias">
+      <div class="social_icons">
+        <img src="fb_ps" alt="facebook">
+        <img src="twitter_ps" alt="twitter">
+        <img src="dribbble_ps" alt="dribbble">
+        <img src="google+_ps" alt="google+">
+        <img src="youtube_ps" alt="youtube">
+      </div>
+      <p>www.Ondin.com </br> contact@odin.com</p>
+    </div>
+  </div>
+  <div class="footer">
+    <p>©2015 - Odin All Right Reserved</p>
+    <p>Unsubscribe</p>
+  </div>
+</div>
---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Newsletter</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;700&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container-fluid" id="header">
+      <img class="logo ml-5" src="logo" alt="logo odin">
+      <ul class="nav">
+        <li class="nav-item"><a class="nav-link" href="#Home">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#Apps">Apps</a></li>
+        <li class="nav-item mr-5"><a class="nav-link" href="#Blog">Blog</a></li>
+      </ul>
+      <div class="text-white">
+        <h1 id="modern">Modern and Clean Newsletter</br>Template for your Agency</h1>
+        <img src="down_arrow" alt="down arrow" id="down_arrow">
+      </div>
+    </div>
+    <div class="container-fluid" id="sections">
+      <div class="row">
+        <div class="col pt-5 pb-5 text-white" id="section1">
+          <img class="pb-4" src="lamp_icon" alt="lamp">
+          <h2>Great Ideas</h2>
+          <p>Creative Minds on Action</p>
+        </div>
+        <div class="col text-white pt-5 pb-5" id="section2">
+          <img class="pb-4" src="tv_icon" alt="tv">
+          <h2>Advertise With Us</h2>
+          <p>We are Big and Loud</p>
+        </div>
+        <div class="col bg-secondary pt-5 pb-5 text-white" id="section3">
+          <img class="pb-4" src="prise_icon" alt="prise">
+          <h2>Work With Us</h2>
+          <p>Work is Our Second Name</p>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid text-left" id="office">
+      <div class="row">
+        <div class="col-6 mt-5" id="we_offer">
+          <h2 >We offer modern and clean</br> newsletter design</h2>
+          <ul>
+            <li>Responsive Design</li>
+            <li>Modern and Clean</li>
+            <li>Help File Included</li>
+          </ul>
+          <a href="#" class="btn text-white" id="learnmore">Learn More</a>
+        </div>
+    <img class="col-6" src="desk1_img.jpg" alt="desk">
+      </div>
+    </div>
+      <div class="container-fluid d-flex" id="video">
+        <div class="row align-items-center">
+          <div class="col justify-content-center">
+            <h1>WATCH OUR VIDEO</h1>
+            <a href="#" class="btn" id="playnow">Play Now</a>
+          </div>
+        </div>
+      </div>
+    <div class="container-fluid pt-3" id="blog">
+      <h1>Some news from our blog</h1>
+      <p>Cras ultricies ligula seo</p>
+      <div class="row no-gutters mt-5">
+        <div class="card text-left offset-md-3" id="post1" style="width: 23rem;">
+          <img class="card-img-top" src="wave_img" alt="wave">
+          <div class="card-body">
+            <h2 class="card-title">Some Random Text</h2>
+            <p class="card-text text-left">Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
+            <a href="#" class="btn btn-dark">Read More</a>
+          </div>
+        </div>
+        <div class="card text-left offset-md-1" id="post2" style="width: 23rem;">
+          <img class="card-img-top" src="seagull_img" alt="seagull">
+          <div class="card-body">
+            <h2 class="card-title">Some Random Text</h2>
+            <p class="card-text text-left">Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
+            <a href="#" class="btn btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+      <img class="mt-4 mb-4" src="plus_icon" alt="plus icon"><!-- insérer bouton +  -->
+    </div>
+    <div class="Advertise bg-dark text-white pt-5 pb-5">
+      <h1>Start your web agency for just $19</h1>
+      <p>Cras ultricies ligula sed magna dictum porta.</p>
+      <a href="#" class="btn mr-3" id="startproject">Start Project</a>
+      <a href="#" class="btn btn-light">Read More</a>
+    </div>
+    <div class="container-fluid pt-5 pb-5" id="contact">
+      <div class="row align-items-center">
+        <div class="col text-left ml-5" id="brand">
+          <img class="mb-3"src="logo" alt="logo odin">
+          <p>Nulla quis lorem ut libero malesuada feugiat.</br>Maurisblandit aliquet elit, eget tincidunt nibh</br>pulvinar est name albis tinchodion esto nih</br>deoslous</p>
+        </div>
+        <div class="col text-left" id="medias">
+          <div class="social_icons">
+            <img class="mr-4" src="fb_ps" alt="facebook">
+            <img class="mr-4" src="twitter_ps" alt="twitter">
+            <img class="mr-4" src="dribbble_ps" alt="dribbble">
+            <img class="mr-4" src="google+_ps" alt="google+">
+            <img src="youtube_ps" alt="youtube">
+            <p>www.Ondin.com </br> contact@odin.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bg-dark text-white pt-4 pb-4" id="footer">
+      <div class="row justify-content-between pt-3">
+        <p class="col-4">©2015 - Odin All Right Reserved</p>
+        <p class="col-4">Unsubscribe</p>
+      </div>
+    </div>
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
