615b166562909f0276c78d8c47110d3e8c8e4e83
pauline-mfr <65295048+pauline-mfr@users.noreply.github.com>
2020-05-25 16:38:01 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,59 +5,79 @@
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Newsletter</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="main">
+    <link rel="stylesheet" type="text/css" href="main.css">
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;700&display=swap" rel="stylesheet">
   </head>
 
   <body>
     <div class="container-fluid" id="header">
-      <img class="logo ml-5" src="logo" alt="logo odin">
-      <ul class="nav">
-        <li class="nav-item"><a class="nav-link" href="#Home">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="#Apps">Apps</a></li>
-        <li class="nav-item mr-5"><a class="nav-link" href="#Blog">Blog</a></li>
-      </ul>
-      <div class="text-white">
+      <div class="row">
+        <div class="col">
+          <img class="logo ml-lg-5 ml-md-2" src="logo.png" alt="logo odin">
+        </div>
+        <div class="col">
+          <nav class="navbar navbar-expand-sm navbar-light">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <a class="nav-link" href="#Home">Home</a>
+          <a class="nav-link" href="#Apps">Apps</a>
+          <a class="nav-link mr-5" href="#Blog">Blog</a>
+        </div>
+        </nav>
+    </div>
+  </div>
+  <div class="row align-items-center justify-content-center" id="titre">
+    <div class="col text-secondary" >
         <h1 id="modern">Modern and Clean Newsletter</br>Template for your Agency</h1>
-        <img src="down_arrow" alt="down arrow" id="down_arrow">
       </div>
     </div>
+    <div class="row align-items-center">
+      <div class="col align-selft-end">
+        <img src="down_arrow" alt="down arrow.png" id="down_arrow">
+    </div>
+  </div>
+</div>
+
+
     <div class="container-fluid" id="sections">
       <div class="row">
-        <div class="col pt-5 pb-5 text-white" id="section1">
-          <img class="pb-4" src="lamp_icon" alt="lamp">
-          <h2>Great Ideas</h2>
+        <div class="col-sm pt-5 pb-5 text-white" id="section1">
+          <img class="pb-4" src="lamp_icon.png" alt="lamp">
+          <h2 style="font-size:1.2rem;">Great Ideas</h2>
           <p>Creative Minds on Action</p>
         </div>
-        <div class="col text-white pt-5 pb-5" id="section2">
-          <img class="pb-4" src="tv_icon" alt="tv">
-          <h2>Advertise With Us</h2>
+        <div class="col-sm text-white pt-5 pb-5" id="section2">
+          <img class="pb-4" src="tv_icon.png" alt="tv">
+          <h2 style="font-size:1.2rem;">Advertise With Us</h2>
           <p>We are Big and Loud</p>
         </div>
-        <div class="col bg-secondary pt-5 pb-5 text-white" id="section3">
-          <img class="pb-4" src="prise_icon" alt="prise">
-          <h2>Work With Us</h2>
+        <div class="col-sm bg-secondary pt-5 pb-5 text-white" id="section3">
+          <img class="pb-4" src="prise_icon.png" alt="prise">
+          <h2 style="font-size:1.2rem;">Work With Us</h2>
           <p>Work is Our Second Name</p>
         </div>
       </div>
     </div>
     <div class="container-fluid text-left" id="office">
       <div class="row">
-        <div class="col-6 mt-5" id="we_offer">
+        <div class="col mt-5 text-secondary offset-lg-1 offset-sm-2" id="we_offer">
           <h2 >We offer modern and clean</br> newsletter design</h2>
           <ul>
             <li>Responsive Design</li>
             <li>Modern and Clean</li>
             <li>Help File Included</li>
           </ul>
-          <a href="#" class="btn text-white" id="learnmore">Learn More</a>
+          <a href="#" class="btn text-white ml-md-4 mb-5" id="learnmore">Learn More</a>
         </div>
-    <img class="col-6" src="desk1_img.jpg" alt="desk">
+          <div class="col" id="desk">
+          </div>
       </div>
     </div>
       <div class="container-fluid d-flex" id="video">
         <div class="row align-items-center">
-          <div class="col justify-content-center">
+          <div class="col" id="watch">
             <h1>WATCH OUR VIDEO</h1>
             <a href="#" class="btn" id="playnow">Play Now</a>
           </div>
@@ -66,17 +86,25 @@
     <div class="container-fluid pt-3" id="blog">
       <h1>Some news from our blog</h1>
       <p>Cras ultricies ligula seo</p>
-      <div class="row no-gutters mt-5">
-        <div class="card text-left offset-md-3" id="post1" style="width: 23rem;">
-          <img class="card-img-top" src="wave_img" alt="wave">
+      <div class="card-deck mt-5">
+        <div class="card text-left" id="post1">
+          <img class="card-img-top" src="post1_img.jpg" alt="wave">
           <div class="card-body">
             <h2 class="card-title">Some Random Text</h2>
             <p class="card-text text-left">Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
             <a href="#" class="btn btn-dark">Read More</a>
           </div>
         </div>
-        <div class="card text-left offset-md-1" id="post2" style="width: 23rem;">
-          <img class="card-img-top" src="seagull_img" alt="seagull">
+        <div class="card text-left" id="post2">
+          <img class="card-img-top" src="post2_img.jpg" alt="seagull">
+          <div class="card-body">
+            <h2 class="card-title">Some Random Text</h2>
+            <p class="card-text text-left">Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
+            <a href="#" class="btn btn-dark">Read More</a>
+          </div>
+        </div>
+        <div class="card text-left" id="post3">
+          <img class="card-img-top" src="post3_img.jpg" alt="wave">
           <div class="card-body">
             <h2 class="card-title">Some Random Text</h2>
             <p class="card-text text-left">Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliue elit, eget tincidunt nibh pulvinar est name ambis tinchodion.</p>
@@ -84,36 +112,40 @@
           </div>
         </div>
       </div>
-      <img class="mt-4 mb-4" src="plus_icon" alt="plus icon"><!-- insérer bouton +  -->
+      <img class="mt-4 mb-4" src="plus_icon.png" alt="plus icon">
     </div>
-    <div class="Advertise bg-dark text-white pt-5 pb-5">
+<div class="text-white pt-sm-5 pb-sm-5" id="pub">
+  <div id="adv">
       <h1>Start your web agency for just $19</h1>
       <p>Cras ultricies ligula sed magna dictum porta.</p>
-      <a href="#" class="btn mr-3" id="startproject">Start Project</a>
-      <a href="#" class="btn btn-light">Read More</a>
+      <a href="#" class="btn mr-sm-3 text-white mt-3 mt-sm-0" id="start">Start Project</a>
+      <a href="#" class="btn btn-light mt-3 mt-sm-0">Read More</a>
     </div>
-    <div class="container-fluid pt-5 pb-5" id="contact">
-      <div class="row align-items-center">
-        <div class="col text-left ml-5" id="brand">
-          <img class="mb-3"src="logo" alt="logo odin">
-          <p>Nulla quis lorem ut libero malesuada feugiat.</br>Maurisblandit aliquet elit, eget tincidunt nibh</br>pulvinar est name albis tinchodion esto nih</br>deoslous</p>
+    </div>
+    <div class="container pt-5 pb-5" id="contact">
+      <div class="row">
+        <div class="col-md-5 offset-md-1 text-left text-secondary" id="brand">
+          <img class="mb-3"src="logo.png" alt="logo odin">
+          <p>Nulla quis lorem ut libero malesuada feugiat.</br>Mauris <span>blandit aliquet elit</span>, eget tincidunt nibh</br>pulvinar est name albis tinchodion esto nih</br>deoslous</p>
         </div>
-        <div class="col text-left" id="medias">
-          <div class="social_icons">
-            <img class="mr-4" src="fb_ps" alt="facebook">
-            <img class="mr-4" src="twitter_ps" alt="twitter">
-            <img class="mr-4" src="dribbble_ps" alt="dribbble">
-            <img class="mr-4" src="google+_ps" alt="google+">
-            <img src="youtube_ps" alt="youtube">
-            <p>www.Ondin.com </br> contact@odin.com</p>
-          </div>
+        <div class="col-md-5 offset-md-1 mt-3 text-left" id="medias">
+          <div class="social_icons pt-md-2 mb-4">
+            <img class="mr-4" src="fb_ps.png" alt="facebook">
+            <img class="mr-4" src="twitter_ps.png" alt="twitter">
+            <img class="mr-4" src="dribbble_ps.png" alt="dribbble">
+            <img class="mr-4" src="google+_ps.png" alt="google+">
+            <img src="youtube_ps.png" alt="youtube">
         </div>
+      <div>
+          <p>www.Ondin.com </br>contact@odin.com</p>
       </div>
-    </div>
-    <div class="container-fluid bg-dark text-white pt-4 pb-4" id="footer">
-      <div class="row justify-content-between pt-3">
-        <p class="col-4">©2015 - Odin All Right Reserved</p>
-        <p class="col-4">Unsubscribe</p>
+      </div>
+      </div>
+        </div>
+    <div class="container-fluid text-white pt-md-4 pb-md-4 pt-sm-3 pt-sm-3" id="footer">
+      <div class="row justify-content-between pt-md-3 pt-2">
+        <p class="col-sm-5 col-8">©2015 - Odin All Right Reserved</p>
+        <p class="col-sm-4 offset-sm-3 col-4"><span>Unsubscribe</span></p>
       </div>
     </div>
 
